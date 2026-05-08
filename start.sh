9e62aba821f5b023c2e9c501565892d735008508
Shaxodat <shakhodat2008@icloud.com>
2026-05-08 17:07:24 +0500

    backup my code

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+gunicorn app.main:app \
+  --workers 4 \
+  --worker-class uvicorn.workers.UvicornWorker \
+  --bind 0.0.0.0:${PORT:-8000} \
+  --timeout 120 \
+  --keep-alive 5 \
+  --log-level info
